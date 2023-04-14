430f62e3bfde5e2da7c71d9ae7c8eae8e1dbe852
Nick <nsagias@gmail.com>
2023-04-13 20:49:21 -0400

    setup quryclient provider and create wait function

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useMutation, useQuery, QueryClientProvider } from "@tanstack/react-query"
 
-function App() {
+
+const POSTS =  [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" }
+];
+
+export default function App() {
+  // takes a unique key for queryKey, must be in array
+  // takes a query function
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS])
+  })
+
+  // check if loading
+  if (postQuery.isLoading) return <h1> Loading ... </h1>
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Here
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+
+function wait (duration: number) {
+  return new Promise(resolve => setTimeout(resolve, duration));
+}
